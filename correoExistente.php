957e83d1b6793632a5f8c54810110abd42e72908
Eduardo20210681LALO <148399985+Eduardo20210681LALO@users.noreply.github.com>
2024-02-09 13:26:05 -0600

    Update correoExistente.php

--- correoExistente.php
+++ correoExistente.php
@@ -5,10 +5,10 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Credentials: true");
 header("Content-Type: application/json");
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "telesecundaria";
+$servername = 'http://srv871.hstgr.io';
+$username = 'u524156408_backendteam';
+$password = 'LaBarbada1979';
+$dbname = 'u524156408_labarbadadev';
 
 $data = json_decode(file_get_contents('php://input'), true);
 $correo = $data['correo'];
